821a167552ccc1b7d6b749ce64657662a26f863f
Haseeb325 <haseebbaig6594325@gmail.com>
2026-03-29 16:49:37 +0500

    change list return response

--- FYPBackend/myapp/utils/pagination.py
+++ FYPBackend/myapp/utils/pagination.py
@@ -12,12 +12,10 @@ class StandardResultsSetPagination(PageNumberPagination):
         return Response(
             {
                 "message": "Success",
-                "data": {
-                    "count": self.page.paginator.count,
-                    "next": self.get_next_link(),
-                    "previous": self.get_previous_link(),
-                    "results": data,
-                },
+                "data": data,
+                "count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
                 "status": status.HTTP_200_OK,
             },
             status=status.HTTP_200_OK,
