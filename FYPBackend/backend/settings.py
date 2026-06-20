f356917d2d8b78f78d71ccb47b18e0962663da88
Haseeb325 <haseebbaig6594325@gmail.com>
2026-06-20 17:19:05 +0500

    change to Brevo

--- FYPBackend/backend/settings.py
+++ FYPBackend/backend/settings.py
@@ -243,13 +243,13 @@ CELERY_ENABLE_UTC = True
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.environ.get("BREVO_SMTP_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("BREVO_SMTP_KEY")
+DEFAULT_FROM_EMAIL = os.environ.get("BREVO_SMTP_USER")
 
 # EMAIL_HOST = "smtp.gmail.com"
 # EMAIL_PORT = 587
