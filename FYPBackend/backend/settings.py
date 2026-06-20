2f98152540c7122286335adf0e3221aa3a4aafa8
Haseeb325 <haseebbaig6594325@gmail.com>
2026-06-20 16:33:16 +0500

    fixes

--- FYPBackend/backend/settings.py
+++ FYPBackend/backend/settings.py
@@ -245,8 +245,9 @@ CELERY_ENABLE_UTC = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.resend.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = "resend"
 EMAIL_HOST_PASSWORD = os.environ.get("RESEND_API_KEY")
 DEFAULT_FROM_EMAIL = "onboarding@resend.dev"
